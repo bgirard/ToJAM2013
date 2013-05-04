624a4b32cc0132a1eeb5418d88751df3c01ae6ac
secretrobotron <secretrobotron@gmail.com>
2013-05-04 12:39:58 -0400

    console typing

--- js/bossos.js
+++ js/bossos.js
@@ -1,8 +1,10 @@
 (function(){
 
+  var COMPY_PRINT_SPEED = 10;
+
   window.BossOs = function (initOptions) {
     var hudContainer = initOptions.hudContainer;
-    var consoleTextArea = hudContainer.querySelector('.hud-element.console > textarea');
+    var consoleTextArea = hudContainer.querySelector('.hud-element.console > .textarea');
 
     var playerKeyCodes = initOptions.playerKeyCodes;
     var playerKeyStates = initOptions.playerKeyStates;
@@ -31,7 +33,17 @@
     });
 
     function writeConsoleLine (str) {
-      consoleTextArea.innerHTML = str + consoleTextArea.innerHTML;
+      var line = document.createElement('div');
+      var strIndex = 0;
+      line.className = 'line';
+      line.innerHTML = '';
+      consoleTextArea.insertBefore(line, consoleTextArea.firstChild);
+      var lineInterval = setInterval(function(){
+        line.innerHTML = line.innerHTML + str[strIndex++];
+        if (strIndex === str.length) {
+          clearInterval(lineInterval);
+        }
+      }, COMPY_PRINT_SPEED);
     }
 
     this.update = function (playerEntity) {
@@ -60,8 +72,22 @@
       return false;
     };
 
-    writeConsoleLine("BossOS v0.29 (c) 2184 -- DO NOT DISTRIBUTE");
-    writeConsoleLine("");
+    var bossScript = [
+      [1, function() { writeConsoleLine('BossOS v0.29 (c) 2184 -- DO NOT DISTRIBUTE\n'); }],
+      [2, function() { writeConsoleLine('Loading kernel modules...'); }],
+      [2, function() { writeConsoleLine('Restoring consciousness. DO NOT POWER OFF.'); }]
+    ];
+
+    function stepBossAI () {
+      var step = bossScript.shift();
+      console.log(step);
+      if (step) {
+        step[1]();
+        setTimeout(stepBossAI, step[0] * 1000); // convert from ms -> seconds
+      }
+    }
+
+    stepBossAI();
   };
 
 }());
