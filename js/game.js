faa49c1aaf9921d25959a55958c0b1caf64364f4
Alan Kligman <ack@mozilla.com>
2013-05-03 21:28:12 -0400

    r+

--- js/game.js
+++ js/game.js
@@ -1,11 +1,56 @@
 (function(){
 
-  function Game() {
+  function extend(obj) {
+    each(slice.call(arguments, 1), function(source) {
+      if (source) {
+        for (var prop in source) {
+          obj[prop] = source[prop];
+        }
+      }
+    });
+    return obj;
+  };
 
-  }
+  var nextEntityId = 0;
+  function Entity(options) {
+    options = options || {};
 
-  Game.prototype = {
+    var id = options['id'] || 'Entity' + nextEntityId++;
+    var img = options['img'];
+    var width = options['width'] || 10;
+    var height = options['height'] || 10;
 
+    var div = document.createElement('div');
+    div.classList.add('Entity');
+    div.id = id;
+    if(img) {
+      div.style.backgroundImage = "url(" + img + ")";
+    } else {
+      div.style.backgroundColor = "red";
+    }
+    div.style.width = width + 'px';
+    div.style.height = height + 'px';
+
+    div.x = 0;
+    div.y = 0;
+    div.velX = 0;
+    div.velY = 0;
+
+    div.update = function update(dt) {
+      div.x += dt * div.velX;
+      div.y += dt * div.velY;
+    };
+
+    div.render = function render() {
+      div.style.left = div.x + 'px';
+      div.style.top = div.y + 'px';
+    };
+
+    return div;
+  };
+
+  var Game = {
+    Entity: Entity
   };
 
   window.Game = Game;
--- js/main.js
+++ js/main.js
@@ -2,6 +2,29 @@
 
   window.onload = function (e) {
     console.log('game start!');
+
+    var entity = new Game.Entity();
+    document.body.appendChild(entity);
+
+    var frame = 0;
+    var cachedTime = Date.now();
+    function main() {
+      window.requestAnimationFrame(main);
+      var t = Date.now();
+      var dt = t - cachedTime;
+      var i;
+
+      var entities = document.getElementsByClassName('Entity');
+      for(i = 0, l = entities.length; i < l; ++ i) {
+        entities[i].update(dt);
+      }
+      for(i = 0, l = entities.length; i < l; ++ i) {
+        entities[i].render();
+      }
+
+      cachedTime = t;
+    };
+    window.requestAnimationFrame(main);
   };
 
 }());
