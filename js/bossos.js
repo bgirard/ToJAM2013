5363160a7575b7b457874f1da25a10dd21d33854
secretrobotron <secretrobotron@gmail.com>
2013-05-04 00:57:55 -0400

    some thruster hud element functionality

--- js/bossos.js
+++ js/bossos.js
@@ -7,12 +7,21 @@
     var playerKeyStates = initOptions.playerKeyStates;
     var playerKeyMap = initOptions.playerKeyStates;
 
+
+    var leftThruster = hudContainer.querySelector('.left.thruster');
+    var rightThruster = hudContainer.querySelector('.right.thruster');
+
     function writeConsoleLine (str) {
       consoleTextArea.innerHTML = str + consoleTextArea.innerHTML;
     }
 
     this.update = function () {
     };
+
+    setInterval(function(){
+      leftThruster.querySelector('.front').style.height = Math.random() * 100 + '%';
+      rightThruster.querySelector('.front').style.height = Math.random() * 100 + '%';
+    }, 2000);
 
     writeConsoleLine("BossOS v0.29 (c) 2184 -- DO NOT DISTRIBUTE");
     writeConsoleLine("");
