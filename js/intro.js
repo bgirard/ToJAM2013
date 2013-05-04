6032d7d73391f93cd33469f9770f55e6dbb52657
secretrobotron <secretrobotron@gmail.com>
2013-05-03 23:15:40 -0400

    some intro work

--- js/intro.js
+++ js/intro.js
@@ -1,3 +1,27 @@
 (function(){
   
+  var scenes = Array.prototype.slice.call(document.querySelectorAll('section'));
+  var currentScene;
+
+  function exitCurrentScene () {
+    nextScene();
+  }
+
+  function nextScene () {
+    currentScene = scenes[scenes.indexOf(currentScene) + 1];
+
+    if (currentScene) {
+      console.log('starting scene', scenes.indexOf(currentScene));
+
+      var duration = currentScene.getAttribute('data-duration') * 1000;
+
+      setTimeout(function(){
+        exitCurrentScene();
+      }, duration);      
+    }
+
+  }
+
+  nextScene();
+
 }());
