547ab137e22974bd33eeed498d9769253f0070cf
Benoit Girard <bgirard@mozilla.com>
2013-05-05 18:43:35 -0400

    level5 progress

--- game/level5.js
+++ game/level5.js
@@ -1,5 +1,59 @@
 (function() {
 
+function spawnWave(div) {
+  div.currentWave++;
+  if (true || div.currentWave == 1) {
+    [
+      new Game.Entity({
+        classes: ['Pirate'],
+        x: 1750,
+        y: 700,
+        width: 80,
+        height: 62,
+        life: 100,
+        img: "images/ships/enemy1.png",
+        spriteFrameTime: 100, //ms
+        spriteFrameX: 0,
+        //spriteMaxFrameX: 4,
+        update: Game.logic.default,
+        ai: Game.logic.ai,
+      })
+    , new Game.Entity({
+        classes: ['Pirate'],
+        x: 2000,
+        y: 650,
+        width: 80,
+        height: 62,
+        life: 900,
+        scaling: 2,
+        bulletType: "BulletStrong",
+        img: "images/ships/enemy2.png",
+        spriteFrameTime: 100, //ms
+        spriteFrameX: 0,
+        //spriteMaxFrameX: 4,
+        update: Game.logic.default,
+        ai: Game.logic.ai,
+      })
+    , new Game.Entity({
+        classes: ['Pirate'],
+        x: 2250,
+        y: 700,
+        width: 80,
+        height: 62,
+        life: 100,
+        img: "images/ships/enemy1.png",
+        spriteFrameTime: 100, //ms
+        spriteFrameX: 0,
+        //spriteMaxFrameX: 4,
+        update: Game.logic.default,
+        ai: Game.logic.ai,
+      })
+    ].forEach(function(entity) {
+      div.appendChild(entity);
+    });
+  }
+}
+
 var id = 'level1';
 function level() {
 
@@ -8,6 +62,18 @@ function level() {
   div.id = id;
   div.levelNo = 5;
   div.nextId = 'level1';
+  div.currentWave = 0;
+
+  var timeInLevel = 0;
+  var timeUntilNextWave = 1000;
+  div.update = function(dt) {
+    timeInLevel += dt;
+    timeUntilNextWave -= dt;
+    if (timeUntilNextWave < 0) {
+      spawnWave(div);
+      timeUntilNextWave = 30000;
+    }
+  };
 
   [
     new Game.Entity({
