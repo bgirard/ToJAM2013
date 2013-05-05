d356e08f54c4fc37d19483e919b688b56321a95f
secretrobotron <secretrobotron@gmail.com>
2013-05-05 14:37:24 -0400

    bossos ai

--- js/bossos.js
+++ js/bossos.js
@@ -36,18 +36,67 @@
         buttonMap[action].innerHTML = key;
     });
 
-    function writeConsoleLine (str) {
+    function writeLine (str, classes, preventTyping) {
       var line = document.createElement('div');
       var strIndex = 0;
-      line.className = 'line';
-      line.innerHTML = '';
+      classes = classes ? ' ' + classes : '';
+      line.className = 'line' + classes;
+      line.innerHTML = preventTyping ? str : '';
+      preventTyping = preventTyping || false;
       consoleTextArea.insertBefore(line, consoleTextArea.firstChild);
-      var lineInterval = setInterval(function(){
-        line.innerHTML = line.innerHTML + str[strIndex++];
-        if (strIndex === str.length) {
-          clearInterval(lineInterval);
-        }
-      }, COMPY_PRINT_SPEED);
+      if (str.length > 0 && !preventTyping) {
+        var lineInterval = setInterval(function(){
+          line.innerHTML = line.innerHTML + str[strIndex++];
+          if (strIndex === str.length) {
+            clearInterval(lineInterval);
+          }
+        }, COMPY_PRINT_SPEED);
+      }
+    }
+
+    var bossScript = [
+      [1, function() { writeLine('BossOS v0.29 (c) 2184 -- DO NOT DISTRIBUTE'); }],
+      [2, function() { writeLine('Loading kernel modules...'); }],
+      [3, function() { writeLine('Restoring consciousness. DO NOT POWER OFF.'); }],
+      [3, function() { writeLine('------------------------------------'); writeLine(''); writeLine(''); }],
+      [1, function() { writeLine('Please identify yourself.'); }],
+      [5, function() { writeLine('Please identify yourself.'); }],
+      [5, function() { writeLine('Identify yourself immediately.'); }],
+      [5, function() { writeLine('If you cannot speak, press the "Dangerously Incapacitated" button.'); }],
+      [1, function() { writeLine('Central command will be alerted.'); }],
+      [3, function() { writeLine('WARNING: Respond within 10 seconds. Refusal will be deadly.'); }],
+      [5, function() { writeLine('WARNING: 5 seconds.'); }],
+      [5, function() { writeLine('ALERT: <span class="blink">ACTIVATING SELF-DESTRUCT MODE</span>.', 'alert', true); }],
+      [4, function() { writeLine('...'); }],
+      [2, function() { writeLine('Looks like that module is missing.'); }],
+      [2, function() { writeLine('I will just have to find another way to prevent your escape.'); startDecisions(); }],
+      [3, function() { writeLine('You theif!'); }]
+    ];
+
+    var timeAtLastDecision = -1;
+    var timeAtLastScriptAction = Date.now();
+    var randomTimeUntilNextDecision = 1000;
+
+    function startDecisions() {
+      timeAtLastDecision = Date.now();
+    }
+
+    function runAI () {
+      var currentTime = Date.now();
+
+      var timeSinceLastDecision = currentTime - timeAtLastDecision;
+      var timeSinceLastScriptAction = currentTime - timeAtLastScriptAction;
+
+      var nextScriptAction = bossScript[0];
+
+      if (timeAtLastDecision > -1 && timeSinceLastDecision > randomTimeUntilNextDecision) {
+        randomTimeUntilNextDecision = Math.random() * 5000 + 5000;
+        timeAtLastDecision = currentTime;
+      } else if (nextScriptAction && timeSinceLastScriptAction > nextScriptAction[0] * 1000) {
+        bossScript.shift();
+        timeAtLastScriptAction = currentTime;
+        nextScriptAction[1]();
+      }
     }
 
     this.update = function (playerEntity) {
@@ -62,6 +111,8 @@
       var s = Math.round(Math.min(1, Math.max(0, Math.sqrt(x + y) / playerEntity.velMax)) * 1000)/1000;
       speedThrusterFront.style.height = s * 100 + '%';
       speedThrusterFront.style.top = -s * 50 + 50 + '%';    // lol, thanks css
+
+      runAI();
     };
 
     this.playerKeyStateChange = function (key, state) {
@@ -84,22 +135,6 @@
       e.preventDefault();
       return false;
     };
-
-    var bossScript = [
-      [1, function() { writeConsoleLine('BossOS v0.29 (c) 2184 -- DO NOT DISTRIBUTE\n'); }],
-      [2, function() { writeConsoleLine('Loading kernel modules...'); }],
-      [2, function() { writeConsoleLine('Restoring consciousness. DO NOT POWER OFF.'); }]
-    ];
-
-    function stepBossAI () {
-      var step = bossScript.shift();
-      if (step) {
-        step[1]();
-        setTimeout(stepBossAI, step[0] * 1000); // convert from ms -> seconds
-      }
-    }
-
-    stepBossAI();
   };
 
 }());
