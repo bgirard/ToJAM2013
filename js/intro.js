d57b5d3b5ee6bd35907616d93f589ea0ecf963a2
secretrobotron <secretrobotron@gmail.com>
2013-05-05 01:36:31 -0400

    intro effects

--- js/intro.js
+++ js/intro.js
@@ -14,6 +14,42 @@
     }, TIME_BETWEEN_SCENES);
   }
 
+  function writeTransition (element, transition) {
+    element.style.webkitTransition = transition;
+    element.style.WebkitTransition = transition;
+    element.style.mozTransition = transition;
+    element.style.MozTransition = transition;
+    element.style.transition = transition;
+  }
+
+  function setupCurrentScene (duration) {
+    var ins = currentScene.querySelectorAll('[data-in]');
+    var outs = currentScene.querySelectorAll('[data-out]');
+
+    var parallax = currentScene.querySelectorAll('.parallax');
+
+    Array.prototype.forEach.call(ins, function (item) {
+      setTimeout(function(){
+        item.classList.add('on');
+      }, item.getAttribute('data-in') * 1000);
+    });
+
+    Array.prototype.forEach.call(outs, function (item) {
+      setTimeout(function(){
+        item.classList.remove('on');
+      }, item.getAttribute('data-out') * 1000);
+    });
+
+    Array.prototype.forEach.call(parallax, function (item) {
+      writeTransition(item, 'right ' + duration/1000 + 's');
+      setTimeout(function(){
+        item.style.right = item.getAttribute('data-ro');
+      }, 0);
+    });
+
+    currentScene.classList.add('on');
+  }
+
   function nextScene () {
     currentScene = scenes[scenes.indexOf(currentScene) + 1];
 
@@ -22,7 +58,7 @@
 
       var duration = currentScene.getAttribute('data-duration') * 1000;
 
-      currentScene.classList.add('on');
+      setupCurrentScene(duration);
 
       setTimeout(function(){
         exitCurrentScene();
